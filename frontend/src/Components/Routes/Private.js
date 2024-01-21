3d7a3ddf1b913a7305ecdac0e7484401373b61c8
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-01-21 15:42:30 +0530

    initial commit

---
+++ frontend/src/Components/Routes/Private.js
@@ -0,0 +1,31 @@
+import { useEffect,useState } from "react";
+import { useAuth } from "../../contetxt/auth";
+import { Outlet } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../spinner";
+export default function PrivateRoute()
+{
+    const [ok,setOk]= useState(false)
+    const {auth,setAuth}=useAuth()
+
+    useEffect(()=>{
+        const authCheck = async()=>{
+            const res = await axios.get('/api/v1/auth/user-auth',
+            {
+                headers:{
+                    "Authorization":auth?.token
+                }
+            })
+            if(res.data.ok)
+            {
+                setOk(true)
+            }
+            else{
+                setOk(false)
+            }
+        };
+        if(auth?.token) authCheck();
+
+    },[auth?.token]);
+    return ok ? <Outlet/> : <Spinner/>;
+}
